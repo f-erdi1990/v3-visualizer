3e1e1fca8121168a4208ac5653189eea9ed9ee2b
f-erdi1990 <ferdinand.ehrhardt@googlemail.com>
2022-01-24 10:26:30 +0100

    add server = app.server

--- visualize.py
+++ visualize.py
@@ -8,6 +8,7 @@ from helper import Sheets
 # style & format
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 # get data
 data = Sheets("v3-optimizer-data").df
